73909273a85d70fbeeb2d565f7383b1ea58e60e6
Mert Sefa AKGUN <git@msakg.com>
2023-04-06 22:52:27 +0300

    added: plugins (see below)
    - catppuccin
    - copilot
    - vim-visual-multi
    - vim-rainbow
    - vim-wakatime
    - i3-vim-syntax

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,73 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+    },
+  },
+  { import = "astrocommunity.colorscheme.nightfox",      enabled = false },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+    -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
+
+  { import = "astrocommunity.pack.rust" },
+
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
+
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+
+  -- Personal Extensions
+  {
+    "wakatime/vim-wakatime",
+    lazy = false
+  },
+
+  {
+    "frazrepo/vim-rainbow"
+  },
+
+  {
+    "mg979/vim-visual-multi",
+    version = master,
+    lazy = false
+  },
+
+  "PotatoesMaster/i3-vim-syntax",
+
 }
